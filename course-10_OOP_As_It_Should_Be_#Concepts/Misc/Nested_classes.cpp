ffb17161e8cf6fe0a7a9a62be70480b3eadfa511
MohamedMustafa84 <mohamedmustofo@gmail.com>
2025-11-05 16:47:02 +0200

    Adding The Lessons Of Neted Classes And  Structure inside the class

---
+++ course-10_OOP_As_It_Should_Be_#Concepts/Misc/Nested_classes.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class clsPerson
+{
+    // Nested  Class
+    class  clsAddress
+    {
+        public :
+        string AddressLine1;
+        string AddressLine2;
+        string City;
+        string Country;
+    };
+
+public:
+    string FullName;
+    clsAddress Address;
+    clsPerson()
+    {
+        FullName = "Mohamed Mustafa";
+        Address.AddressLine1 = "UmKaty";
+        Address.AddressLine2 = "Karary";
+        Address.City = "Khartoum";
+        Address.Country = "Sudan";
+    }
+    void Print()
+    {
+        cout << "================= Person Information ==================" << endl;
+        cout << "Full Name     : " << FullName << endl;
+        cout << "Address Line1 : " << Address.AddressLine1 << endl;
+        cout << "Address Line2 : " << Address.AddressLine2 << endl;
+        cout << "City          : " << Address.City << endl;
+        cout << "Country       : " << Address.Country << endl;
+        cout << "========================================================" << endl;
+    }
+};
+
+int main()
+{
+
+    clsPerson Person1;
+    Person1.Print();
+    cout <<"\nThe Best Country In The World IS : "<<Person1.Address.Country;
+
+    return 0;
+}
