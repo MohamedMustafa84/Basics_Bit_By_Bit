fd44a22113c88f269398da2f0c2023aafdafbae5
MohamedMustafa84 <mohamedmustofo@gmail.com>
2025-12-13 20:59:38 +0200

    add git node byIndex , update item by index And Insert item after node by index

--- course-13_problem-solving-level-5/Project_One/Project_One/main.cpp
+++ course-13_problem-solving-level-5/Project_One/Project_One/main.cpp
@@ -56,9 +56,8 @@ int main(){
     // cout << "\n\nAfter deleting the Last node \n";
     // MyDblLinkedList.printList();
 
-    
-    
-    
+    // ---------------------------------------------------------------------------------------------------
+
     // Extention 1 : Add size Method
     
     // MyDblLinkedList.InsertAtBeginning(5);
@@ -71,10 +70,9 @@ int main(){
     // MyDblLinkedList.printList();
     
     // cout << "\n the number of items in the linked list  = " << MyDblLinkedList.Size();
-    
-    
-    
-    
+
+    // ---------------------------------------------------------------------------------------------------
+
     // Extention 2 : Add method To Check If The linked List Empty
     
     // cout << "is the linked list Empty ? \n";
@@ -86,11 +84,9 @@ int main(){
     // MyDblLinkedList.InsertAtBeginning(1);
     // cout << "\nAfter Adding Some Element  is it still empty ? \n" << endl;
     // cout << (MyDblLinkedList.IsEmpty()? "Yes List is Empty \n" : "No List is Not Empty \n");
-    
-    
-    
-    
-    
+
+    // ---------------------------------------------------------------------------------------------------
+
     // Extention 3 : Clear the linked list
     
     // MyDblLinkedList.InsertAtBeginning(3);
@@ -107,9 +103,9 @@ int main(){
     
     // MyDblLinkedList.printList();
     // cout << "Number of Items in the linked list is " << MyDblLinkedList.Size();
-    
-    
-    
+
+    // ---------------------------------------------------------------------------------------------------
+
     // Extention 4 : Reverse the linked list
     
     // MyDblLinkedList.InsertAtBeginning(5);
@@ -126,23 +122,83 @@ int main(){
     // cout << "\n The linked list After the Reverse :\n";
     // MyDblLinkedList.printList();
 
+    // ---------------------------------------------------------------------------------------------------
 
     // Extention 5 : Get Node By Index
 
+    // MyDblLinkedList.InsertAtBeginning(5);
+    // MyDblLinkedList.InsertAtBeginning(4);
+    // MyDblLinkedList.InsertAtBeginning(3);
+    // MyDblLinkedList.InsertAtBeginning(2);
+    // MyDblLinkedList.InsertAtBeginning(1);
+
+    // cout <<"Linked List Content :\n";
+    // MyDblLinkedList.printList();
+
+    // clsDblLinkedList<int>::node *NodeToFind;
+
+    // NodeToFind = MyDblLinkedList.GetNode(3);
+
+    // cout << "the node at the index 3 is " << NodeToFind->value << endl;
+
+    // ---------------------------------------------------------------------------------------------------
+    // Extention 6 : Get Item By Index
+
+    // MyDblLinkedList.InsertAtBeginning(5);
+    // MyDblLinkedList.InsertAtBeginning(4);
+    // MyDblLinkedList.InsertAtBeginning(3);
+    // MyDblLinkedList.InsertAtBeginning(2);
+    // MyDblLinkedList.InsertAtBeginning(1);
+
+    // cout <<"Linked List Content :\n";
+    // MyDblLinkedList.printList();
+
+    // cout << "Item 2 value is " << MyDblLinkedList.GetItem(2) << endl;
+
+
+
+    // ---------------------------------------------------------------------------------------------------
+
+    // Extention 7 : Update Item By Index
+
+    // MyDblLinkedList.InsertAtBeginning(5);
+    // MyDblLinkedList.InsertAtBeginning(4);
+    // MyDblLinkedList.InsertAtBeginning(500);
+    // MyDblLinkedList.InsertAtBeginning(2);
+    // MyDblLinkedList.InsertAtBeginning(1);
+
+    // cout <<"Linked List Content :\n";
+    // MyDblLinkedList.printList();
+
+    // MyDblLinkedList.Update(2, 3);
+
+    // cout <<"Linked List After Updating the item of the Index 2  " <<endl;
+    // MyDblLinkedList.printList();
+
+
+    
+    // ---------------------------------------------------------------------------------------------------
+
+    // Extention 8 : Insert After node By Index  
+
     MyDblLinkedList.InsertAtBeginning(5);
     MyDblLinkedList.InsertAtBeginning(4);
-    MyDblLinkedList.InsertAtBeginning(3);
+    MyDblLinkedList.InsertAtBeginning(500);
     MyDblLinkedList.InsertAtBeginning(2);
     MyDblLinkedList.InsertAtBeginning(1);
 
     cout <<"Linked List Content :\n";
     MyDblLinkedList.printList();
 
-    clsDblLinkedList<int>::node *NodeToFind;
+    MyDblLinkedList.InsertAfter(2, 3);
 
-    NodeToFind = MyDblLinkedList.GetNode(3);
+    cout <<"Linked List After Updating the item of the Index 2  " <<endl;
+    MyDblLinkedList.printList();
 
-    cout << "the node at the index 3 is " << NodeToFind->value << endl;
+
+
+
+
 
     return 0;
 }
