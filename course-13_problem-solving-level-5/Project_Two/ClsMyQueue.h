64dbf3572697ad744a2ede9d50a4c0b9de228953
MohamedMustafa84 <mohamedmustofo@gmail.com>
2025-12-17 18:18:01 +0200

    Add 7 extentions to MyQueue library : get item by index ,reverse queue,update item by index ,inert After ,Insert At Front ,Insert At Back And Clear Queue Extention

--- course-13_problem-solving-level-5/Project_Two/ClsMyQueue.h
+++ course-13_problem-solving-level-5/Project_Two/ClsMyQueue.h
@@ -38,4 +38,33 @@ class clsMyQueue{
         {
             return _MyList.GetItem(size()-1);
         }
+
+        T GetItem(int Index){
+            return _MyList.GetItem(Index);
+        }
+
+        void Reverse(){
+            _MyList.Reverse();
+        }
+
+        void UpdateItem(int Index , T Value){
+            _MyList.Update(Index, Value);
+        }
+
+        void InsertAfter(int Index, T Value){
+            _MyList.InsertAfter(Index, Value);
+        }
+
+        void InsertAtFront(T Value ){
+            _MyList.InsertAtBeginning(Value);
+        }
+
+        void InsertAtBack(T Value)
+        {
+            _MyList.InsertAtEnd(Value);
+        }
+
+        void Clear(){
+            _MyList.Clear();
+        }
 };
