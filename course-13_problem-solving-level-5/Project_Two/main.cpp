296fa93e12e8d44b1b008e60f56fa8466d296e3b
MohamedMustafa84 <mohamedmustofo@gmail.com>
2025-12-17 18:18:01 +0200

    Add 7 extentions to MyQueue library : get item by index ,reverse queue,update item by index ,inert After ,Insert At Front ,Insert At Back And Clear Queue Extention

--- course-13_problem-solving-level-5/Project_Two/main.cpp
+++ course-13_problem-solving-level-5/Project_Two/main.cpp
@@ -28,5 +28,50 @@ int main(){
     cout << "\nMy Queue After pop() :\n";
     MyQueue.print();
 
+    // Extention #1
+
+    cout << "\n\n Item(2)" << MyQueue.GetItem(2);
+
+    // Extention #2
+    // MyQueue.Reverse();
+
+    cout << "\n Queue After the reverse : \n";
+    MyQueue.print();
+    
+    // Extention #3
+    MyQueue.UpdateItem(2, 500);
+    
+    cout <<"\nQueue After Updating the Item of Index 2 To 500 :\n";
+    MyQueue.print();
+    
+    // Extention #4
+    
+    MyQueue.InsertAfter(3, 400);
+    
+    cout << "Queue after inserting 400 after the item of index 3 : \n";
+    MyQueue.print();
+    
+    
+    
+    // Extention #5
+    MyQueue.InsertAtFront(100);
+
+    cout << "Queue After Inserting 100 at the front :\n";
+    MyQueue.print();
+
+    // Extention #6
+    MyQueue.InsertAtBack(200);
+    
+    cout << "Queue After Inserting 200 at the back :\n";
+    MyQueue.print();
+    
+    // Extention #7
+    MyQueue.Clear();
+    
+    cout << "Queue After Clear() : \n";
+    MyQueue.print();
+    
+    
+
     return 0;
 }
