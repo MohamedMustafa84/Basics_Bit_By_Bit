a0721a95d7604efc69c54662d89952ed60be7dcf
MohamedMustafa84 <mohamedmustofo@gmail.com>
2025-12-27 16:53:20 +0200

    Add Project Seven (Undo && Redo Project)

---
+++ course-13_problem-solving-level-5/Project_Seven/ClsMyString.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class clsMyString{
+    stack<string> _Undo;
+    stack<string> _Redo;
+    string _Value = "";
+
+    public:
+
+    void SetValue(string value){
+        _Undo.push(value);
+        _Value = value;
+    }
+
+    string Value(){
+        return _Value;
+    }
+
+    void Undo(){
+        if(!_Undo.empty()){
+            _Redo.push(_Value);
+            _Value = _Undo.top();
+            _Undo.pop();
+        }
+    }
+
+
+    void Redo(){
+        if(!_Redo.empty()){
+            _Undo.push(_Value);
+            _Value = _Redo.top();
+            _Redo.pop();
+        }
+    }
+
+
+};
