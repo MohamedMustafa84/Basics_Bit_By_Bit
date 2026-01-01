d927ed3b96a7b752fbf81c0f5f1448c7439572de
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-01-01 20:04:26 +0200

    Add Show Clients List Screen Feature

---
+++ course-11_OOP_As_It_Should_Be_#Applications/Bank_System_Project/Ui_Main_Menu/ClsScreen.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class clsScreen{
+    protected:
+        static void _DrawScreenHeader(string Title ,string SubTitle =""){
+            cout << "\n\n\n\t\t\t\t--------------------------------------------";
+            cout << "\n\t\t\t\t\t" << Title;
+            
+            if(SubTitle!=""){
+                cout<< "\n\t\t\t\t\t" << SubTitle;
+            }
+            cout << "\n\t\t\t\t--------------------------------------------\n";
+        }
+};
