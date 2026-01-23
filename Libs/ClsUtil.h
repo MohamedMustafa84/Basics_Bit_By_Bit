e259c5ac833684a56d147f58834b4f2cf092c44a
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-01-23 06:26:43 +0200

    Add a condtion to prevent any user from modifying admin data

--- Libs/ClsUtil.h
+++ Libs/ClsUtil.h
@@ -202,25 +202,25 @@ class clsUtil{
         return t;
     }
 
-    static string EncryptText(string Text, short EncryptionKey=4)
-    {
-
-        for (int i = 0; i <= Text.length(); i++)
-        {
-
-            Text[i] = char((int)Text[i] - EncryptionKey);
-        }
-
-        return Text;
-    }
-
-    static string DecryptText(string Text, short EncryptionKey=4)
+    static string EncryptText(string Text, short EncryptionKey=2)
     {
 
         for (int i = 0; i <= Text.length(); i++)
         {
 
             Text[i] = char((int)Text[i] + EncryptionKey);
+        }
+
+        return Text;
+    }
+
+    static string DecryptText(string Text, short EncryptionKey=2)
+    {
+
+        for (int i = 0; i <= Text.length(); i++)
+        {
+
+            Text[i] = char((int)Text[i] - EncryptionKey);
         }
         return Text;
     }
