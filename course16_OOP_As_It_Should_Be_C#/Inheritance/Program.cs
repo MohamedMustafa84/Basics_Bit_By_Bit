5681c14d668ba85b93e25db6a9f12eb28d02d57a
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-05-15 08:24:23 +0200

    Add Inheritance With constructor lesson

--- course16_OOP_As_It_Should_Be_C#/Inheritance/Program.cs
+++ course16_OOP_As_It_Should_Be_C#/Inheritance/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
 
-class clsPerson
+ public class clsPerson
 {
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Title{ get; set; }
+
+
+     public clsPerson (int ID , string FirstName, string LastName)
+    {
+        Id = ID ;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+    }
 
     
     public string FullName()
@@ -19,7 +26,15 @@ class clsPerson
 // Employee inherit person 
 
 
-class clsEmployee : clsPerson {
+  class clsEmployee : clsPerson {
+
+
+
+    public clsEmployee (int ID ,string FirstName, string LastName , float Salary, string JobTitle) : base(ID, FirstName, LastName)
+    {
+        jobTitle = JobTitle;
+        this.Salary = Salary;
+    }
 
 // here you can access the Public and Protected Members only  from the class clsPerson
     public float  Salary { get; set; }
@@ -40,24 +55,44 @@ internal class Program
     static void Main(string[] args)
     {
 
-        clsEmployee Employee1 = new clsEmployee();
+        //clsEmployee Employee1 = new clsEmployee();
 
-        Employee1.Id = 1;
-        Employee1.FirstName = "Mohamed";
-        Employee1.LastName = "Mustafa";
-        Employee1.Title = "Backend developer";
-        Employee1.Salary = 10000;
 
-        // here you can acces clsperson public members  Only
+        //Employee1.Id = 1;
+        //Employee1.FirstName = "Mohamed";
+        //Employee1.LastName = "Mustafa";
+        //Employee1.Title = "Backend developer";
+        //Employee1.Salary = 10000;
 
-        Console.WriteLine("Employee ID  :{0}",Employee1.Id);
-        Console.WriteLine("Employee Full Name  :{0}",Employee1.FullName());
-        Console.WriteLine("Employee Job Title  :{0}",Employee1.jobTitle);
-        Console.WriteLine("Employee Salary     :{0}", Employee1.Salary);
+        //// here you can acces clsperson public members  Only
 
-        Employee1.IncreaseSalaryBy(100);
+        //Console.WriteLine("Employee ID  :{0}",Employee1.Id);
+        //Console.WriteLine("Employee Full Name  :{0}",Employee1.FullName());
+        //Console.WriteLine("Employee Job Title  :{0}",Employee1.jobTitle);
+        //Console.WriteLine("Employee Salary     :{0}", Employee1.Salary);
 
-        Console.WriteLine("Salary After increased By 100 Usd : {0} Usd",Employee1.Salary);
+        //Employee1.IncreaseSalaryBy(100);
+
+        //Console.WriteLine("Salary After increased By 100 Usd : {0} Usd",Employee1.Salary);
+
+
+
+
+        //----------------
+
+        // inhertiance with the constructors 
+
+        clsEmployee Employee2 = new clsEmployee(1,"Mohamed","Mustafa",10000,"Backend Developer");
+
+
+        Console.WriteLine("Employee ID  :{0}", Employee2.Id);
+        Console.WriteLine("Employee Full Name  :{0}", Employee2.FullName());
+        Console.WriteLine("Employee Job Title  :{0}", Employee2.jobTitle);
+        Console.WriteLine("Employee Salary     :{0}", Employee2.Salary);
+
+        Employee2.IncreaseSalaryBy(100);
+
+        Console.WriteLine("Salary After increased By 100 Usd : {0} Usd", Employee2.Salary);
 
 
 
