1e9dd6b22570994dc14decc51f42b71ba1d3a0e4
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-06-02 17:16:50 +0200

    Add PartialClasses And Partial Methods

---
+++ course16_OOP_As_It_Should_Be_C#/PartialClassesAndPartialMethods/ClsMyClass1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+public partial class ClsMyClass { 
+
+    public  void Method1()
+    {
+        Console.WriteLine("Method One Is Called");
+    }
+
+}
+
+
+
+public  partial class ClsPerson { 
+
+    public short Age { get; set; }
+
+    // Partial Method
+    partial void PrintAge();
+
+    public void BirthDay()
+    {
+        Age++;
+        // if PrintAge Is Impelemented it will be Called OtherWise it will Ignore 
+        PrintAge();
+    }
+}
