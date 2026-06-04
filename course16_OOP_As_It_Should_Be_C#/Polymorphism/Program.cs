15deb184da6609ef0228986f3ff317fb68d13490
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-06-04 17:14:25 +0200

    Add Polymorphism Lesson

--- course16_OOP_As_It_Should_Be_C#/Polymorphism/Program.cs
+++ course16_OOP_As_It_Should_Be_C#/Polymorphism/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Polymorphism :
+
+// refer to Multiple form i,e Object at different types can be treted as object of common based type.
+
+// C# Support two type of Polymorphism:
+
+
+// 1- compile time polymorphism (Also known as method overloading ) :
+
+// method overloading allow multiple method to have the same name but with different prameters
+// the compiler  select the appropriate method  to call based on number , type, and order of prameters
+
+
+
+// 2- Run Time Polymorphism (also known as overriding ) :
+
+//  method overriding allow the subclass  to provide specific implementataion of a method that already provided by the parent class 
+// the method in the subclass most have the same signature (name , retrun type, prameters) as the method in the parent class 
+
+
+
+
+// Note  :
+ // inhertaince is also form of polymorphism known as subtyping or subtype polymorphism
+
