161cf8dcb458db8072529c934f3c12a88da4a36d
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-06-05 09:02:55 +0200

    Add Date time Picker And Month Calender Controll

---
+++ course14_C#_Level_One/WindowsForm/DateTimePickerAndMonthCalender.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace PIZZA_ORDER
+{
+    partial class DateTimePickerAndMonthCalender
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dateTimePicker1 = new DateTimePicker();
+            button1 = new Button();
+            DateTimeFormate = new Label();
+            lbDateTimePickerFormat = new Label();
+            monthCalendar1 = new MonthCalendar();
+            button3 = new Button();
+            button4 = new Button();
+            button2 = new Button();
+            tAthkarTimer = new System.Windows.Forms.Timer(components);
+            lbAthkar = new Label();
+            button5 = new Button();
+            button6 = new Button();
+            tBlankTimer = new System.Windows.Forms.Timer(components);
+            notifyIcon1 = new NotifyIcon(components);
+            button7 = new Button();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = " ";
+            dateTimePicker1.Location = new Point(12, 28);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 0;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(27, 100);
+            button1.Name = "button1";
+            button1.Size = new Size(148, 23);
+            button1.TabIndex = 1;
+            button1.Text = "show Short Date";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // DateTimeFormate
+            // 
+            DateTimeFormate.AutoSize = true;
+            DateTimeFormate.Location = new Point(348, 55);
+            DateTimeFormate.Name = "DateTimeFormate";
+            DateTimeFormate.Size = new Size(0, 15);
+            DateTimeFormate.TabIndex = 2;
+            // 
+            // lbDateTimePickerFormat
+            // 
+            lbDateTimePickerFormat.AutoSize = true;
+            lbDateTimePickerFormat.Location = new Point(272, 64);
+            lbDateTimePickerFormat.Name = "lbDateTimePickerFormat";
+            lbDateTimePickerFormat.Size = new Size(38, 15);
+            lbDateTimePickerFormat.TabIndex = 3;
+            lbDateTimePickerFormat.Text = "label1";
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(425, 28);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(566, 270);
+            button3.Name = "button3";
+            button3.Size = new Size(86, 23);
+            button3.TabIndex = 6;
+            button3.Text = "Start ";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(684, 270);
+            button4.Name = "button4";
+            button4.Size = new Size(104, 23);
+            button4.TabIndex = 7;
+            button4.Text = "End";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(415, 270);
+            button2.Name = "button2";
+            button2.Size = new Size(105, 23);
+            button2.TabIndex = 9;
+            button2.Text = "selected Range";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // tAthkarTimer
+            // 
+            tAthkarTimer.Interval = 1000;
+            tAthkarTimer.Tick += TAthkarTimer_Tick;
+            // 
+            // lbAthkar
+            // 
+            lbAthkar.AutoSize = true;
+            lbAthkar.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbAthkar.Location = new Point(159, 194);
+            lbAthkar.Name = "lbAthkar";
+            lbAthkar.Size = new Size(67, 32);
+            lbAthkar.TabIndex = 10;
+            lbAthkar.Text = "اذكار ";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(61, 270);
+            button5.Name = "button5";
+            button5.Size = new Size(86, 23);
+            button5.TabIndex = 11;
+            button5.Text = "ابدأ";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(209, 270);
+            button6.Name = "button6";
+            button6.Size = new Size(86, 23);
+            button6.TabIndex = 12;
+            button6.Text = "توقف";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // tBlankTimer
+            // 
+            tBlankTimer.Interval = 950;
+            tBlankTimer.Tick += tBlankTimer_Tick;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            notifyIcon1.BalloonTipClosed += notifyIcon1_BalloonTipClosed;
+            notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(159, 360);
+            button7.Name = "button7";
+            button7.Size = new Size(173, 23);
+            button7.TabIndex = 13;
+            button7.Text = "show notify icon";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // DateTimePickerAndMonthCalender
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(lbAthkar);
+            Controls.Add(button2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(monthCalendar1);
+            Controls.Add(lbDateTimePickerFormat);
+            Controls.Add(DateTimeFormate);
+            Controls.Add(button1);
+            Controls.Add(dateTimePicker1);
+            Name = "DateTimePickerAndMonthCalender";
+            Text = "DateTimePicker";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private Button button1;
+        private Label DateTimeFormate;
+        private Label lbDateTimePickerFormat;
+        private MonthCalendar monthCalendar1;
+        private Button button3;
+        private Button button4;
+        private Button button2;
+        private System.Windows.Forms.Timer tAthkarTimer;
+        private Label lbAthkar;
+        private Button button5;
+        private Button button6;
+        private System.Windows.Forms.Timer tBlankTimer;
+        private NotifyIcon notifyIcon1;
+        private Button button7;
+    }
+}
---
+++ course14_C#_Level_One/WindowsForm/DateTimePickerAndMonthCalender.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIZZA_ORDER
+{
+    public partial class DateTimePickerAndMonthCalender : Form
+    {
+        public DateTimePickerAndMonthCalender()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(dateTimePicker1.Value.ToShortDateString());
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            lbDateTimePickerFormat.Text = dateTimePicker1.Value.ToString("dd-MM-yyyy") + Environment.NewLine;
+
+            lbDateTimePickerFormat.Text += dateTimePicker1.Value.ToString("dddd-MM-yyyy") + Environment.NewLine;
+
+            lbDateTimePickerFormat.Text += dateTimePicker1.Value.ToString("MM-dd-yyyy") + Environment.NewLine;
+
+            lbDateTimePickerFormat.Text += dateTimePicker1.Value.ToString("dd-MM-yy") + Environment.NewLine;
+            lbDateTimePickerFormat.Text += dateTimePicker1.Value.ToString("dddd,dd-MM-yyyy") + Environment.NewLine;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(monthCalendar1.SelectionRange.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(monthCalendar1.SelectionStart.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(monthCalendar1.SelectionEnd.ToString());
+        }
+
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            tAthkarTimer.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            tAthkarTimer.Enabled = true;
+            tBlankTimer.Enabled = true;
+
+            counter = 1;
+            thkerCounter = 1;
+
+        }
+
+        private void tBlankTimer_Tick(object sender, EventArgs e)
+        {
+            lbAthkar.Text = "";
+        }
+
+
+
+
+        private short counter = 1;
+        private short thkerCounter = 1;
+        private void PrintThker(string Thiker)
+        {
+
+
+            if (thkerCounter == 100)
+            {
+
+                lbAthkar.Text = Thiker;
+                tBlankTimer.Enabled = false;
+                tAthkarTimer.Enabled = false;
+            }
+            else
+            {
+                lbAthkar.Text = Thiker + " -" + counter.ToString();
+
+            }
+
+
+
+        }
+
+        private void TAthkarTimer_Tick(object sender, EventArgs e)
+        {
+
+            string Thiker = "";
+
+            if (thkerCounter <= 33)
+            {
+
+                Thiker = "استغفر الله";
+
+
+            }
+            else if (thkerCounter > 33 && thkerCounter <= 66)
+            {
+                if (thkerCounter == 34)
+                {
+                    counter = 1;
+                }
+
+                Thiker = "الحمد لله";
+            }
+            else if (thkerCounter > 66 && thkerCounter <= 99)
+            {
+                if (thkerCounter == 67)
+                {
+                    counter = 1;
+                }
+
+                Thiker = "الله اكبر";
+
+            }
+
+            if (thkerCounter == 100)
+            {
+                Thiker = "لا اله الا الله ";
+            }
+
+
+
+            PrintThker(Thiker);
+
+            counter++;
+            thkerCounter++;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Icon = SystemIcons.Application;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipTitle = "This Is A Title";
+            notifyIcon1.BalloonTipText = "This IS Message";
+            notifyIcon1.ShowBalloonTip(20000);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("You Respond To The NotifyIcon And That Distraction" ,"Future Self");
+        }
+
+        private void notifyIcon1_BalloonTipClosed(object sender, EventArgs e)
+        {
+            MessageBox.Show("thinks for a voiding the Distration and ignoring this Notify ballloon ", " Future Self ");
+        }
+    }
+}
