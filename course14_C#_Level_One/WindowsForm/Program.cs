4462068fdbe05c502309700247bfad6578b96947
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-06-05 09:13:42 +0200

    Add Check List Box And Date Time Piker Controlls

--- course14_C#_Level_One/WindowsForm/Program.cs
+++ course14_C#_Level_One/WindowsForm/Program.cs
@@ -13,7 +13,7 @@ namespace PIZZA_ORDER
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new TicTacToeGame());
+            Application.Run(new WinFormControls());
         }
     }
 }
---
+++ course14_C#_Level_One/WindowsForm/checkListBoxAndDateTimePiker.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace PIZZA_ORDER
+{
+    partial class checkListBox_DateTimePiker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkedListBox1 = new CheckedListBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "option1", "option2 ", "option3 ", "option4" });
+            checkedListBox1.Location = new Point(3, 17);
+            checkedListBox1.MultiColumn = true;
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(213, 94);
+            checkedListBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 180);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Add Item";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(93, 180);
+            button2.Name = "button2";
+            button2.Size = new Size(161, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Show Checked Items";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(280, 180);
+            button3.Name = "button3";
+            button3.Size = new Size(104, 23);
+            button3.TabIndex = 3;
+            button3.Text = "check All Items";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 232);
+            button4.Name = "button4";
+            button4.Size = new Size(132, 23);
+            button4.TabIndex = 4;
+            button4.Text = "un check All Items";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(178, 232);
+            button5.Name = "button5";
+            button5.Size = new Size(104, 23);
+            button5.TabIndex = 5;
+            button5.Text = "clear";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(114, 290);
+            button6.Name = "button6";
+            button6.Size = new Size(168, 23);
+            button6.TabIndex = 6;
+            button6.Text = "Remove third Item";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // checkListBox_DateTimePiker
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkedListBox1);
+            Name = "checkListBox_DateTimePiker";
+            Text = "checkListBox_DateTimePiker";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private CheckedListBox checkedListBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
