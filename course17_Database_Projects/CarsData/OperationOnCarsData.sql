0a1519915f008cb2778409b8405715eb4ea631a7
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-06-19 05:24:33 +0200

    Add problem 10 (Get all vehicle that Runs with GAS

--- course17_Database_Projects/CarsData/OperationOnCarsData.sql
+++ course17_Database_Projects/CarsData/OperationOnCarsData.sql
@@ -112,5 +112,12 @@
 
 
 
-
+		
+			  
+		--peoblem 10 : get all Makes that runs with GAS
+		select  distinct Makes.Make,FuelTypes.FuelTypeName
+		from VehicleDetails
+		inner join Makes on VehicleDetails.MakeID =Makes.MakeID
+		inner join  FuelTypes on VehicleDetails.FuelTypeID = FuelTypes.FuelTypeID
+		where FuelTypes.FuelTypeName =N'GAS';
 			
