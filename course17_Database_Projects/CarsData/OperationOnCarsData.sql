33badc358f5981a70fd42e52741133b04546ad52
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-06-22 05:44:33 +0200

    add problem 30  get all vehicle display name  num doors and add extra column  to describe number of dors by words and if door is null display not set

--- course17_Database_Projects/CarsData/OperationOnCarsData.sql
+++ course17_Database_Projects/CarsData/OperationOnCarsData.sql
@@ -338,3 +338,21 @@
 					exists(
 								select top 1 * from vehicleDetails where Year =1950
 							)
+
+
+			--problem 30 : get all vehicle display name  num doors and add extra column  to describe number of dors by words and if door is null display not set 
+
+			select VehicleDetails.Vehicle_Display_Name ,VehicleDetails.NumDoors ,
+				CASE
+						when NumDoors=0 then 'Zero Doors'
+						when NumDoors=1 then 'One Door'
+						when NumDoors =2 then 'Two Doors'
+						when NumDoors=3 then 'Three Doors'
+						when NumDoors=4 then 'Four Doors'
+						when NumDoors= 5 then 'Five Doors'
+						when NumDoors=6 then 'Six Doors'
+						when NumDoors=8 then 'Eight Doors'
+						when NumDoors is null then 'Not Set'
+						else 'unknown'
+			END as DoorDescription
+				from VehicleDetails;
