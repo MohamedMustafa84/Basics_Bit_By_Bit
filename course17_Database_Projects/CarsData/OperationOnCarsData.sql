5a8f126bf84b145b8cc669a375fd6d227c2b75b2
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-06-25 12:27:38 +0200

    Add problem 42  get a table of unique Engine CC and Calculate tax per Engine

--- course17_Database_Projects/CarsData/OperationOnCarsData.sql
+++ course17_Database_Projects/CarsData/OperationOnCarsData.sql
@@ -458,3 +458,22 @@
 				)order by makes.Make;
 
 
+		-- problem 42 : get a table of unique Engine CC and Calculate tax per Engine
+
+			select Engine_CC, 
+				Case
+					when Engine_CC between 0 and 1000  then 100 
+					when Engine_CC between 1001 and 2000  then 200
+					when Engine_CC between 2001 and 4000  then 300
+					when Engine_CC between 4001 and 6000  then 400
+					when Engine_CC between 6001 and 8000  then 500
+					when Engine_CC >8000	then 600
+					else 0
+					END as Tax 
+
+				from (
+						select distinct Engine_CC  from VehicleDetails
+					)R1
+					Order By Engine_CC;
+
+
