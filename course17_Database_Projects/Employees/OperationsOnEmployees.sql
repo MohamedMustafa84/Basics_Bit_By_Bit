8c761854260546d8ce55290334a17c6b458cf95f
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-06-25 17:39:33 +0200

    add problem 52 get all employees that have managers or does not have manager  along with managers name  in case no manager name show null

--- course17_Database_Projects/Employees/OperationsOnEmployees.sql
+++ course17_Database_Projects/Employees/OperationsOnEmployees.sql
@@ -4,3 +4,15 @@
 
 select Employees.Name,Employees.ManagerID,Employees.Salary ,Managers.Name as managerName
 		from Employees inner join Employees as Managers on Employees.ManagerID = Managers.ManagerID;
+
+
+
+		
+-- problem 52 : get all employees that have managers or does not have manager  along with managers name  in case no manager name show null
+
+
+select Employees.Name,Employees.ManagerID,Employees.Salary ,Managers.Name as ManagerName
+		from Employees left join Employees as Managers on Employees.ManagerID = Managers.ManagerID
+		
+		
+
