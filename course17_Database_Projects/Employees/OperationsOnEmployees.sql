df98b5c1de4e7867eedb43d6dca4a4f659e40ed0
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-06-25 18:00:28 +0200

    add problem 54 get all employees managed by Ahmed

--- course17_Database_Projects/Employees/OperationsOnEmployees.sql
+++ course17_Database_Projects/Employees/OperationsOnEmployees.sql
@@ -18,7 +18,7 @@ select Employees.Name,Employees.ManagerID,Employees.Salary ,Managers.Name as Man
 
 
 
--- problem 52 : get all employees that have managers or does not have manager  along with managers name
+-- problem 53 : get all employees that have managers or does not have manager  along with managers name
 	--in case no manager name  is the same employee name as a manager to himself 
 	
 	
@@ -32,6 +32,11 @@ select Employees.Name,Employees.ManagerID,Employees.Salary ,
 
 		from Employees left join Employees as Managers on Employees.ManagerID = Managers.ManagerID
 		
+	
+	-- problem 54 : get all employees managed by Ahmed
 		
+		select Employees.Name,Employees.ManagerID,Employees.Salary ,Managers.Name as ManagerName
+		from Employees inner join Employees as Managers on Employees.ManagerID = Managers.ManagerID
+			where Managers.Name ='Ahmed';
 
 
