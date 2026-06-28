fb6b5fa1b0416e50cc495da014adadfa26ffacd0
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-06-28 18:26:44 +0200

    connect to ContactsDB and Get ALL Contacts Using ADO.Net

---
+++ course18_C#_And_Database/ContactDB/ContactDB/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System;
+using System.Net;
+using Microsoft.Data.SqlClient;
+
+
+
+
+
+
+internal class Program{
+
+    static string ConnectionString = "Server=.;Database=ContactsDB;Integrated Security=True;TrustServerCertificate=True"; 
+
+    static  void PrintAllContact()
+    {
+        SqlConnection Connection = new SqlConnection(ConnectionString);
+
+        string query = "Select * from Contacts";
+
+        SqlCommand Command = new SqlCommand(query, Connection);
+
+        try
+        {
+            Connection.Open();
+            SqlDataReader reader =  Command.ExecuteReader();  
+
+            while (reader.Read())
+            {
+                int ContactID = (int)reader["ContactID"];
+                string FirstName = (string)reader["FirstName"];
+                string LastName = (string)reader["LastName"];
+                string Email = (string)reader["Email"];
+                string Phone = (string)reader["Phone"];
+                string Address = (string)reader["Address"];
+                int CountryID = (int)reader["CountryID"];
+
+
+
+                Console.WriteLine($"Contact ID :{ContactID}");
+                Console.WriteLine($"Name :{FirstName}{LastName}");
+                Console.WriteLine($"Email :{Email}");
+                Console.WriteLine($"Phone :{Phone}");
+                Console.WriteLine($"Address :{Address}");
+                Console.WriteLine($"CountryID :{CountryID}");
+
+                Console.WriteLine("--------------------------------------------------------------");
+
+
+
+
+            }
+            reader.Close();
+            Connection.Close();
+
+
+        }catch(Exception ex)
+        {
+            Console.WriteLine("Error : "+ ex.Message);
+        }
+
+
+        
+
+
+    }
+
+
+
+
+    public static void Main()
+    {
+        PrintAllContact();
+        Console.ReadKey();
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
