4a8c44f7bc25304a24b417cf4562273c384799ae
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-07-02 11:38:43 +0200

    Adding two functions to search for contact with the first nam , firstName and countryID

--- course18_C#_And_Database/ContactDB/ContactDB/Program.cs
+++ course18_C#_And_Database/ContactDB/ContactDB/Program.cs
@@ -67,10 +67,116 @@ internal class Program{
 
 
 
+    //Parameterized Query :
+
+
+    static void FindContactWithFirstName(string FirstName)
+    {
+        SqlConnection Connection = new SqlConnection(ConnectionString);
+
+        string query = "SELECT * FROM Contacts WHERE FirstName = @FirstName";
+
+        SqlCommand Command = new SqlCommand(query,Connection);
+
+        Command.Parameters.AddWithValue("@FirstName", FirstName);
+
+
+        try
+        {
+            Connection.Open();
+            SqlDataReader reader = Command.ExecuteReader();
+
+            while(reader.Read())
+            {
+
+                int ContactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string LastName = (string)reader["LastName"];
+                string Email = (string)reader["Email"];
+                string Phone = (string)reader["Phone"];
+                string Address = (string)reader["Address"];
+                int CountryID = (int)reader["CountryID"];
+
+
+
+                Console.WriteLine($"Contact ID :{ContactID}");
+                Console.WriteLine($"Name :{FirstName}{LastName}");
+                Console.WriteLine($"Email :{Email}");
+                Console.WriteLine($"Phone :{Phone}");
+                Console.WriteLine($"Address :{Address}");
+                Console.WriteLine($"CountryID :{CountryID}");
+
+                Console.WriteLine("--------------------------------------------------------------");
+
+            }
+            reader.Close();
+            Connection.Close();
+        }catch (Exception ex)
+        {
+            Console.WriteLine("Error : "+ ex.Message);
+        }
+
+    }
+
+    public  static void PrintAllContactsWithFirstNameAndCountryID (string firstName,int countryID)
+    {
+        SqlConnection Connection = new SqlConnection(ConnectionString);
+
+        string query = "select * from Contacts where @FirstName=firstName and @CountryID = countryID";
+
+        SqlCommand Command = new SqlCommand(query,Connection);
+        Command.Parameters.AddWithValue("@firstName", firstName);
+        Command.Parameters.AddWithValue("@CountryID", countryID);
+        
+        try
+        {
+            Connection.Open();
+            SqlDataReader reader = Command.ExecuteReader();
+
+            while (reader.Read())
+            {
+
+                int ContactID = (int)reader["ContactID"];
+                string FirstName = (string)reader["FirstName"];
+                string LastName = (string)reader["LastName"];
+                string Email = (string)reader["Email"];
+                string Phone = (string)reader["Phone"];
+                string Address = (string)reader["Address"];
+                int CountryID = (int)reader["CountryID"];
+
+
+
+                Console.WriteLine($"Contact ID :{ContactID}");
+                Console.WriteLine($"Name :{FirstName}{LastName}");
+                Console.WriteLine($"Email :{Email}");
+                Console.WriteLine($"Phone :{Phone}");
+                Console.WriteLine($"Address :{Address}");
+                Console.WriteLine($"CountryID :{CountryID}");
+
+                Console.WriteLine("--------------------------------------------------------------");
+
+            }
+
+
+            reader.Close();
+            Connection.Close();
+
+        }catch (Exception ex)
+        {
+            Console.WriteLine("Error : ",ex.Message);
+            
+        }
+    }
+
+
 
     public static void Main()
     {
-        PrintAllContact();
+        //PrintAllContact();
+
+        //FindContactWithFirstName("jane");
+        PrintAllContactsWithFirstNameAndCountryID("jane",2);
+
         Console.ReadKey();
     }
 
