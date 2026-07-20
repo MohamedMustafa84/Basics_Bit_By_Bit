2b6ea36890cb47086c0288e675436bb4b62e3a34
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-07-20 19:14:20 +0200

    Add filter  TO DataTable

--- course18_C#_And_Database/C#DataTable/Program.cs
+++ course18_C#_And_Database/C#DataTable/Program.cs
@@ -16,26 +16,27 @@ internal class Program
         dtEmployees.Columns.Add("Email", typeof(string));
         dtEmployees.Columns.Add("Address", typeof(string));
         dtEmployees.Columns.Add("Phone", typeof(string));
+        dtEmployees.Columns.Add("Country", typeof(string));
         dtEmployees.Columns.Add("Salary", typeof(double));
         dtEmployees.Columns.Add("Date", typeof(DateTime));
 
 
         // Add Data 
 
-        dtEmployees.Rows.Add(1, "Mohamed", "mustafa", "mohameail.com", "khrto12 st 4t", "9049093335", 10000.00, DateTime.Now);
-        dtEmployees.Rows.Add(2, "Maher", "mustafa", "mohamed3ail.com", "khrto12 st 4t", "90490899335", 10500.00, DateTime.Now);
-        dtEmployees.Rows.Add(3, "ali", "mustafa", "jkjkmustaf", "khrtoum 12 st", "9049090035", 1000.00, DateTime.Now);
-        dtEmployees.Rows.Add(4, "Ahmed", "mustafa", "mohamedml.com", "khrtoum st 4t", "9040903335", 900.00, DateTime.Now);
-        dtEmployees.Rows.Add(5, "Modether", "mustafa", "mohama@gmail.com", "koum 12 st 4t", "9049093335", 2000.00, DateTime.Now);
-        dtEmployees.Rows.Add(6, "Taha", "mustafa", "mohamedmuil.com", "khrtou2 st 4t", "9049094335", 15000.00, DateTime.Now);
-        dtEmployees.Rows.Add(7, "Alshier", "mustafa", "mohamea@gmail.com", "koum 12 st 4t", "9009093335", 14400.00, DateTime.Now);
+        dtEmployees.Rows.Add(1, "Mohamed", "mustafa", "mohameail.com", "khrto12 st 4t", "9049093335", "Sudan", 10000.00, DateTime.Now);
+        dtEmployees.Rows.Add(2, "Maher", "mustafa", "mohamed3ail.com", "khrto12 st 4t", "90490899335", "UK", 10500.00, DateTime.Now);
+        dtEmployees.Rows.Add(3, "ali", "mustafa", "jkjkmustaf", "khrtoum 12 st", "9049090035", "Sudan", 1000.00, DateTime.Now);
+        dtEmployees.Rows.Add(4, "Ahmed", "mustafa", "mohamedml.com", "khrtoum st 4t", "9040903335", "USA", 900.00, DateTime.Now);
+        dtEmployees.Rows.Add(5, "Modether", "mustafa", "mohama@gmail.com", "koum 12 st 4t", "9049093335", "Sudan", 2000.00, DateTime.Now);
+        dtEmployees.Rows.Add(6, "Taha", "mustafa", "mohamedmuil.com", "khrtou2 st 4t", "9049094335", "Jordan", 15000.00, DateTime.Now);
+        dtEmployees.Rows.Add(7, "Alshier", "mustafa", "mohamea@gmail.com", "koum 12 st 4t", "9009093335", "KSA", 14400.00, DateTime.Now);
 
         // List Employees :
 
         Console.WriteLine("Employees List ::-\n");
         foreach (DataRow Row in dtEmployees.Rows)
         {
-            Console.WriteLine($"ID :{Row["ID"]}  FullName : {Row["FirstName"]} {Row["LastName"]} Phone :{Row["Phone"]}  Email : {Row["Email"]} Address : {Row["Address"]} Salary : {Row["Salary"]} Date : {Row["Date"]}");
+            Console.WriteLine($"ID :{Row["ID"]}  FullName : {Row["FirstName"]} {Row["LastName"]} Phone :{Row["Phone"]}  Email : {Row["Email"]} Address : {Row["Address"]} Country : {Row["Country"]} Salary : {Row["Salary"]} Date : {Row["Date"]}");
         }
 
         int EmployeesCount = 0;
@@ -59,6 +60,40 @@ internal class Program
         Console.WriteLine("Max Salary  = " + MaxSalary);
 
 
+
+        // Filter Data 
+
+        // Filter By Country Sudan 
+
+        DataRow[] ResultRows = dtEmployees.Select("Country='Sudan'");
+
+        int ResultCount = 0;
+
+        foreach (DataRow Row in ResultRows)
+        {
+
+            Console.WriteLine("Filter Sudanese Employees :-");
+            Console.WriteLine($"ID :{Row["ID"]}  FullName : {Row["FirstName"]} {Row["LastName"]} Phone :{Row["Phone"]}  Email : {Row["Email"]} Address : {Row["Address"]} Country : {Row["Country"]} Salary : {Row["Salary"]} Date : {Row["Date"]}");
+        }
+
+        ResultCount = ResultRows.Count();
+
+        Console.WriteLine("Sudanese Employee Count is " + ResultCount);
+
+        // filter BY Sudan OR Jordan
+
+        ResultRows = dtEmployees.Select("Country='Sudan' or Country='Jordan'");
+
+        foreach (DataRow Row in ResultRows)
+        {
+
+            Console.WriteLine("Filter Sudanese and Jordan Employees :-");
+            Console.WriteLine($"ID :{Row["ID"]}  FullName : {Row["FirstName"]} {Row["LastName"]} Phone :{Row["Phone"]}  Email : {Row["Email"]} Address : {Row["Address"]} Country : {Row["Country"]} Salary : {Row["Salary"]} Date : {Row["Date"]}");
+        }
+
+        double SudaneseTotalSalary = Convert.ToDouble(dtEmployees.Compute("Sum(Salary)", "Country='Sudan"));
+
+        Console.WriteLine("Sudanese Employees Total Salary = " + SudaneseTotalSalary);
 
 
     }
