959b916f5a9af9e368e99c5ae9d5becf7bdc9070
MohamedMustafa84 <mohamedmustofo@gmail.com>
2026-07-26 11:09:23 +0200

    Get The Data from ContactsDB Using DataAdapter

---
+++ course18_C#_And_Database/C#DataAdapter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+internal class Program
+{
+    static void Main(string[])
+    {
+        string ConnectionString = "Server=.;Database=ContactsDB;Integrated Security= True; TrustServerCertificate = True ;";
+
+
+        // Creat New DataSet 
+        DataSet dataSet = new DataSet();
+
+        // Create DataAdapter With A select query and connection String 
+
+        string query = "Select * from Contacts ;";
+
+        DataAdapter dataAdapter = new DataAdapter(query,ConnectionString);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
